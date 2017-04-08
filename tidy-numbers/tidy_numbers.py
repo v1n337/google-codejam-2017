988d137f125378077a5301f99158bce2fd385b6a
Vineet John <vinjohn10@gmail.com>
2017-04-07 21:01:10 -0400

    Another naive implementation

--- tidy-numbers/tidy_numbers.py
+++ tidy-numbers/tidy_numbers.py
@@ -5,10 +5,15 @@ input_file_path = sys.argv[1]
 
 def find_last_tidy(number):
     while (number >= 0):
-        print("working on " + str(number))
+        # print("working on " + str(number))
         number_list = [int(d) for d in str(number)]
         if sorted(number_list) == number_list:
             return number
+        elif sorted(number_list) == list(reversed(number_list)):
+            new_num_str = ""
+            for i in range(len(number_list) - 1):
+                new_num_str += "9"
+            return int(new_num_str)
         number -= 1
 
 
@@ -19,4 +24,4 @@ with open(input_file_path) as input_file:
         number = int(input_file.readline())
 
         tidy_number = find_last_tidy(number)
-        print(tidy_number)
+        print("Case #" + str(test_case + 1) + ": " + str(tidy_number))
